607bf8a98e8c8510c7dfbea1fc018142601df441
Kris Zyp <kriszyp@gmail.com>
2010-02-23 09:52:47 -0700

    Convert appropriate functions from fs module Handle undeclared callback parameter

--- fs-promise.js
+++ fs-promise.js
@@ -3,7 +3,15 @@
 */
 
 var fs = require("fs"),
-  convertNodeAsyncFunction = require("promise").convertNodeAsyncFunction;
-for(var i in fs) {
-  exports[i] = convertNodeAsyncFunction(fs[i]);
+  convertNodeAsyncFunction = require("./promise").convertNodeAsyncFunction;
+
+// convert all the non-sync functions
+for (var i in fs) {
+  if (!(i.match(/Sync$/))) {
+    exports[i] = convertNodeAsyncFunction(fs[i]);
+  }
 }
+
+// convert the functions that don't have a declared callback
+exports.writeFile = convertNodeAsyncFunction(fs.writeFile, true);
+exports.readFile = convertNodeAsyncFunction(fs.readFile, true);
